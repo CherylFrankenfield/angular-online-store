543e62f75fde15caf4b95d65519ac31cf0fce554
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-21 21:40:07 -0800

    Add ngOnInit method with fat arrow notation

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Album } from '../album.model';
 
 @Component({
   selector: 'app-album-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.css']
 })
 export class AlbumDetailComponent implements OnInit {
+  albumId: number = null;
 
-  constructor() { }
-
+//any instance of AlbumDetailComponent will have route/location properties that can be accessed w this.route and this.location.
+  constructor(private route: ActivatedRoute, private location: Location) {}
+//loops through and temporarily assigns each item parameter the variable name urlParameters. Uses Fat arrow notation.
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.albumId = parseInt(urlParameters['id']);
+    });
   }
+//Does not work with familiar/old way of writing .forEach b/c this refers to function itself.
+  // ngOnInit() {
+  //   this.route.params.forEach(function(urlParameters){
+  //    this.albumId = parseInt(urlParameters['id']);
+  //  });
+  // }
 
 }
