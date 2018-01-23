5e50b9c9322d10ed02659c905a6bdaef087fb2cf
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-22 22:00:28 -0800

    Add service, injection and create mock-album file

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -2,22 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
-  albumId: number = null;
+  // albumId: number = null;
+  albumId: number;
+  albumToDisplay: Album;
 
 //any instance of AlbumDetailComponent will have route/location properties that can be accessed w this.route and this.location.
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(private route: ActivatedRoute, private location: Location, private albumService: AlbumService) {}
 //loops through and temporarily assigns each item parameter the variable name urlParameters. Uses Fat arrow notation.
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
-      this.albumId = parseInt(urlParameters['id']);
-    });
+     this.albumId = parseInt(urlParameters['id']);
+   });
+   this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
 //Does not work with familiar/old way of writing .forEach b/c this refers to function itself.
   // ngOnInit() {
---
+++ src/app/album.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+
+@Injectable()
+export class AlbumService {
+
+  constructor() { }
+
+//service may provide this information wherever it's injected
+  getAlbums() {
+    return ALBUMS;
+  }
+//returns specific album
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+}
