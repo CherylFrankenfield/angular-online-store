7b1e630fe934a31048ded0d6fcf6661e594e4718
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-23 20:26:17 -0800

    Add firebase and angularfire, apikey, update appmodule, and albumservice

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
-//service may provide this information wherever it's injected
-  getAlbums() {
-    return ALBUMS;
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
   }
+  
+  getAlbums(){
+    return this.albums;
+  }
+//service may provide this information wherever it's injected
+  // getAlbums() {
+  //   return ALBUMS;
+  // }
 //returns specific album
   getAlbumById(albumId: number){
     for (var i = 0; i <= ALBUMS.length - 1; i++) {
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -11,8 +11,8 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
-  albums: Album[];
-
+  // albums: Album[];
+  albums: FirebaseListObservable<any[]>;
 //private Router object named router. This ensures that every instance of MarketplaceComponent has a Router object available when it is instantiated. this ensures all new instances of MarketplaceComponent also have an instance of AlbumService, accessible by calling this.albumService
   constructor(private router: Router, private albumService: AlbumService) {}
 
